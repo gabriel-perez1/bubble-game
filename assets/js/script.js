e4e60eac2a129a8001e2d7dcdf3f8655c5c35414
gabriel-perez1 <83041381+gabriel-perez1@users.noreply.github.com>
2021-10-30 10:36:32 -0400

    user particle and user/particle function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let particleSystem = [];
 let particleNum = 50;
+let vx = 0, vy = 0; // user particle position
 
 // adds particle to array
 function setup () {
@@ -16,6 +17,27 @@ function draw () {
     particleSystem[i].move();
     particleSystem[i].show();
     particleSystem[i].limit();
+  }
+  // particle that user controls
+  noCursor();
+  fill('red');
+  noStroke();
+  circle(vx, vy, 20);
+
+  if (keyIsDown(LEFT_ARROW)) {
+    vx -= 5;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    vx += 5;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    vy -= 5;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    vy += 5;
   }
 }
 
@@ -45,6 +67,17 @@ class particle {
     }
     if (this.y > height || this.y < 0) {
       this.speed.y = -this.speed.y;    
+    }
+  }
+
+  // function that returns true if particle and vehicle position converge
+  contain(x2, y2)  {
+    let d = dist (x2, y2, this.x, this.y);
+    if (d < 20) { //20 is the size of the particle so if the distance is less than, they're touching
+      return true;
+    }
+    else {
+      return false;
     }
   }
 }
